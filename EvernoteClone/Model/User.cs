6f9970167870f9b692a60c1e91221cb4661ebd52
Muhammad Fazal <38839909+fazaltech@users.noreply.github.com>
2023-08-28 07:46:41 +0500

    The SQLite Functionality

--- EvernoteClone/Model/User.cs
+++ EvernoteClone/Model/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,14 @@ namespace EvernoteClone.Model
 {
 	public class User
 	{
+        [PrimaryKey,AutoIncrement]
         public int ID { get; set; }
+
+        [MaxLength(50)]
         public string Name { get; set; }
-        public string Lastname { get; set; }
+
+		[MaxLength(50)]
+		public string Lastname { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
     }
